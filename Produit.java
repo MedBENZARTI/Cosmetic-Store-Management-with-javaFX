00dcb68a03b4c122b99532c497b6fcd66ef14625
mohamed <albenzarti.mohamed@gmail.com>
2020-04-24 17:14:52 +0100

    creation of Produit class

---
+++ Produit.java
@@ -0,0 +1,85 @@
+
+public class Produit {
+    private int ID_produit;
+    private String Nom_produit;
+    private float Prix_achat;
+    private float Prix_vente;
+    private String Type_produit;
+    private String Marque;
+
+    public int getID_produit() {
+        return ID_produit;
+    }
+
+    public void setID_produit(int iD_produit) {
+        this.ID_produit = iD_produit;
+    }
+
+    public String getNom_produit() {
+        return Nom_produit;
+    }
+
+    public void setNom_produit(String nom_produit) {
+        this.Nom_produit = nom_produit;
+    }
+
+    public float getPrix_achat() {
+        return Prix_achat;
+    }
+
+    public void setPrix_achat(float prix_achat) {
+        this.Prix_achat = prix_achat;
+    }
+
+    public float getPrix_vente() {
+        return Prix_vente;
+    }
+
+    public void setPrix_vente(float prix_vente) {
+        this.Prix_vente = prix_vente;
+    }
+
+    public String getType_produit() {
+        return Type_produit;
+    }
+
+    public void setType_produit(String type_produit) {
+        this.Type_produit = type_produit;
+    }
+
+    public String getMarque() {
+        return Marque;
+    }
+
+    public void setMarque(String marque) {
+        this.Marque = marque;
+    }
+
+    public Produit(int iD_produit, String nom_produit, float prix_achat, float prix_vente, String type_produit,
+            String marque) {
+        this.ID_produit = iD_produit;
+        this.Nom_produit = nom_produit;
+        this.Prix_achat = prix_achat;
+        this.Prix_vente = prix_vente;
+        this.Type_produit = type_produit;
+        this.Marque = marque;
+    }
+
+    public void AfficheDetailsProduit() {
+        System.out.println("Produit: " + this.Nom_produit);
+        System.out.println("ID: " + this.ID_produit);
+        System.out.println("Marque: " + this.Marque);
+        System.out.println("Type: " + this.Type_produit);
+        System.out.println("Prix d'achat: " + this.Prix_achat);
+        System.out.println("Prix de vente: " + this.Prix_vente);
+    }
+
+    public void AfficheProduitTicket() {
+        System.out.println(
+                "Produit: " + this.Nom_produit + " | Code: " + this.ID_produit + " | Prix: " + this.Prix_vente);
+    }
+
+    public void Remise(float r) {
+        this.Prix_vente = r * this.Prix_vente;
+    }
+}
